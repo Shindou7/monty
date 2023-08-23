567626ea1972a1593921402233bb49e8495db2b1
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 16:30:17 +0100

    Create opcode_mod.c

---
+++ opcode_mod.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * mod - computes the rest of the division of the second
+ * @head: stack head
+ * @line_number: line_number
+ * Return: Nothing
+ */
+void mod(stack_t **head, unsigned int line_number)
+{
+	stack_t *flag = *head;
+	unsigned int count = 0;
+
+	    while (flag != NULL)
+      {
+        count++;
+        flag = flag->next;
+      }
+
+	if (count < 2)
+	{
+		dprintf(2, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+  flag = *head;
+	if (flag->n == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	flag->next->n = flag->next->n % flag->n;
+	*head = flag->next;
+	free(flag);
+}
