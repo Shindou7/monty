e2d8d11be9d4be836a894a562f761fc69c99d371
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 16:15:13 +0100

    Create opcode_div.c

---
+++ opcode_div.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ * div - divides the second element by the top element of the stack
+ * @head: stack head
+ * @line_number: line_number
+ *
+ * Return: Nothing
+ */
+void div(stack_t **head, unsigned int line_number)
+{
+	stack_t *flag = *head;
+	unsigned int  count = 0;
+
+	    while (flag != NULL)
+      {
+        count++;
+        flag = flag->next;
+      }
+
+	if (count < 2)
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+flag = *head;
+	if (flag->n == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	flag->next->n = flag->next->n / flag->n;
+	*head = flag->next;
+	free(flag);
+}
