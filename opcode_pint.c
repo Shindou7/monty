00a5f8221ba8e409a5f6df57aad0c38a58ecec51
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 13:45:47 +0100

    Create opcode_pint.c

---
+++ opcode_pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * pint - print int a top of stack,
+ * @head: The head of the stack
+ * @line_number: number of line opcode
+ * Return: Nothing
+ */
+void pint(stack_t **head, unsigned int line_number)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
