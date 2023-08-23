02b47a00ed47e40e7ba52804af115885962e2bc6
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 15:31:20 +0100

    Create opcode_add.c

---
+++ opcode_add.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * add - Adds  the stack
+ * @head: The head of the stack
+ * @line_number: The line on which the error occurred
+ * Return: Nothing
+ */
+void add(stack_t **head, unsigned int line_number)
+{
+	stack_t *flag = *head;
+	unsigned int count = 0;
+
+	    while (flag != NULL)
+      {
+        count++;
+        flag = flag->next;
+      }
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+flag->next->n = flag->n + flag->next->n;
+*head = flag->next;
+free(flag);
+}
