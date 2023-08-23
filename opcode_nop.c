5c6356e7c5002c334ca092e6b07680c6cc57ebba
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 15:37:00 +0100

    Create opcode_nop.c

---
+++ opcode_nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  * nop -  nop top of stack y second top stack
+  * @stack: stack head
+  * @line_number: line_number
+  * Return: Nothing
+  */
+
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void) head;
+	(void) line_number;
+}
