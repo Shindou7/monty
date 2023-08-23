6c97466a8ce39d2cb6a8910300c0de98c6154460
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 16:52:10 +0100

    Create opcode_pstr.c

---
+++ opcode_pstr.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+  * pstr - prints the string starting
+  * @head: The head of the stack
+  * @line_number: The line on which the error occurred
+  * Return: Nothing
+  */
+void pstr(stack_t **head, unsigned int line_number)
+{
+	stack_t *flag;
+	(void)line_number;
+
+	flag = *head;
+
+	while (flag && flag->n > 0 && flag->n < 128)
+	{
+		printf("%c", flag->n);
+		flag = flag->next;
+	}
+
+	printf("\n");
+}
