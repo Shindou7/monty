2aeeb71e75c20dbbe444f3b1f378c4ef7aa28da7
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-24 00:58:33 +0100

    Update opcode_div.c

--- opcode_div.c
+++ opcode_div.c
@@ -20,14 +20,14 @@ void s_div(stack_t **head, unsigned int line_number)
 
 	if (count < 2)
 	{
-		dprintf(2, "L%u: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 flag = *head;
 	if (flag->n == 0)
 	{
-		dprintf(2, "L%u: division by zero\n", line_number);
+		fprintf(stderr, "L%u: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
