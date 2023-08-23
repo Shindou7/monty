ca21250515237fb2ce2b4816b82673db81d1a1eb
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 16:30:37 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -55,6 +55,7 @@ void nop(stack_t **head, unsigned int line_number);
 void sub(stack_t **head, unsigned int line_number);
 void div(stack_t **head, unsigned int line_number);
 void mul(stack_t **head, unsigned int line_number);
+void mod(stack_t **head, unsigned int line_number);
 
 
 
