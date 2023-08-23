8d6e6721d0a7479433e283bdfae3525636a53ccb
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-24 00:08:13 +0100

    Update opcode_pall.c

--- opcode_pall.c
+++ opcode_pall.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 
 /**
- * pall - Prints all the values on the stack
+ * s_pall - Prints all the values on the stack
  *
  * @head: head of the linked list
  * @line_number: line numbers
  * Return: no return
  */
-void pall(stack_t **head, unsigned int line_number)
+void s_pall(stack_t **head, unsigned int line_number)
 {
 	stack_t *montty;
 	(void)line_number;
