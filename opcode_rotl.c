918f449cdc6bd4eb28a18c5405cfaffc57f9532f
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 17:05:18 +0100

    Create opcode_rotl.c

---
+++ opcode_rotl.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+  * rotl - rotates the stack to the top
+  * @head: The head of the stack
+  * @line_number: line_number
+  * Return: Nothing
+  */
+
+void rotl(stack_t **head, unsigned int line_number)
+{
+  stack_t *flag = *head, *new_head = NULL;
+	(void) line_number;
+
+	if (flag && flag->next)
+	{
+    new_head = flag->next;
+		while (flag->next != NULL)
+		{
+      flag = flag->next;	
+		}
+		flag->next = *head;
+		*head = new_head;
+		new_head->next = NULL;
+	}
+}
