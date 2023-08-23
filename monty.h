34d1d253470cb381b8349888bc8a463756d8d176
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-23 13:46:06 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -47,6 +47,7 @@ typedef struct instruction_s
 
 void pall(stack_t **head, unsigned int line_number);
 void push(stack_t **head, unsigned int line_number);
+void pint(stack_t **head, unsigned int line_number);
 
 
 
